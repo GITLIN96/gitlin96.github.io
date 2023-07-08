18ca859865b0d1d3f91d9385f6e66f10f21264db
GITLIN96 <lindyso0706@gmail.com>
2023-07-08 12:35:21 +0800

    some padding

--- js/script.js
+++ js/script.js
@@ -17,6 +17,26 @@ function cookieVal(cookieName) {
  
 document.getElementById('result').innerHTML = "<p>You visited this page <label style='font-size:16px;' class='text-info'>"+counter+"</label> times.</p>";
 
+// Get the button
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
 // var prevScrollPos = window.pageYOffset;
 // console.log(prevScrollPos);
 // window.onscroll = function() {
